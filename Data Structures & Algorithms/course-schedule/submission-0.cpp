da4c7d9a00d4956bef0c8045cf78b0b4e900ba63
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-06 20:11:50 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void dfs(unordered_map<int,vector<int>>&mp,int i,bool& res,set<int>&visited){
+        vector<int> prq = mp[i];
+        if(!prq.size()){
+            return;
+        }
+        if(visited.find(i) != visited.end()){
+            res = true;
+            return;
+        }
+        visited.insert(i);
+        for(int j=0;j<prq.size();j++){
+            dfs(mp,prq[j],res,visited);
+        }
+        visited.erase(i);
+
+
+
+
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+       unordered_map<int,vector<int>>mp;
+       bool res = false;
+       for(int i=0;i<numCourses;i++){
+        mp[i] = {};
+       }
+       for(int i=0;i<prerequisites.size();i++){
+        mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+       }
+       set<int> visited;
+       for(auto i:mp){
+        dfs(mp,i.first,res,visited);
+        if(res){
+            return false;
+        }
+       }
+       return true;
+
+
+
+        
+    }
+};
