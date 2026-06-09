a5278d09a28ee6037df0faf7a6dfd6855de15c61
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-09 20:42:30 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    unordered_map<int,bool> visited;
+    int dfs(unordered_map<int,vector<int>>&mp,int i,int p,set<int>& visit){
+        visited[i] = true;
+        if(visit.find(i)!= visit.end()){
+            return 0;
+        }
+        visit.insert(i);
+        for(int j=0;j<mp[i].size();j++){
+            if(mp[i][j]!=p){
+                 dfs(mp,mp[i][j],i,visit);
+            }
+           
+        }
+        // visit.erase(i);
+        return 1;
+
+
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<edges.size();i++){
+            mp[edges[i][0]].push_back(edges[i][1]);
+            mp[edges[i][1]].push_back(edges[i][0]);
+     
+        }
+        set<int>visit;
+        int count =0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(mp,i,-1,visit);
+                count++;
+            }
+        }
+        return count;
+
+     
+
+
+
+    }
+};
