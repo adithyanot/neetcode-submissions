8ba68ad001ed86b8ae24117820734e0a72e9f8da
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-26 15:17:55 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countones(int n){
+        int count =0;
+        while(n){
+            count+= n&1;
+            n = n >>1;
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int> res(n+1);
+        for(int i=0;i<=n;i++){
+            res[i] = countones(i);
+        }
+        return res;
+    }
+};
