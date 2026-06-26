1bda8d5062e642b8649240bb3de34b229f647b05
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-26 20:24:29 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++){
+            dp[0][i] = i;
+        }
+        for(int i=0;i<=m;i++){
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int add = dp[i][j-1]+1;
+                    int sub = dp[i-1][j]+1;
+                    int rep = dp[i-1][j-1]+1;
+                    dp[i][j] = min(add,min(sub,rep));
+
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
