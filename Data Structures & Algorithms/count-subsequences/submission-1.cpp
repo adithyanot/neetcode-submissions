71063b6375141c23fdee11789405a20b22aa3ab3
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-26 19:56:17 +0530

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++){
+            dp[i][0] = 1;
+
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(t[j-1] == s[i-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                cout<<dp[i][j]<<", ";
+
+            }
+            cout<<endl;
+        }
+        return dp[m][n];
+
+        
+        
+    }
+};
