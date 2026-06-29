3d0373bed119711a78ec4d251e30d2c4892becf4
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-29 18:15:48 +0530

    Add: regular-expression-matching - submission-3

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+        dp[0][0] = true;
+        for(int j=0;j<n;j++){
+            if(p[j]=='*'){
+                dp[0][j+1] = dp[0][j-1];
+            }
+
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(p[j] == '.'){
+                    dp[i+1][j+1] = dp[i][j];
+                }else if(p[j] == '*'){
+                    if(p[j-1]=='.'){
+                        dp[i+1][j+1] = true;
+                    }else if(i-1>=0 && s[i] == s[i-1] &&s[i] == p[j-1]){
+                        dp[i+1][j+1] = dp[i+1][j] || dp[i][j] || dp[i][j+1]|| dp[i+1][j-1] ;
+
+                    }else{
+                        dp[i+1][j+1] = dp[i+1][j]|| dp[i+1][j-1];
+                    }
+                }else{
+                    if(s[i]==p[j]){
+                        dp[i+1][j+1] = dp[i][j];
+                    }else{
+                        dp[i+1][j+1] = false;
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+};
