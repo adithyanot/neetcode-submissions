6da71a5848a7cdb83729d3287de18ccc150a86c5
adithyanot <96722526+adithyanot@users.noreply.github.com>
2026-06-30 18:06:19 +0530

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l=0;
+        int r =0;
+        int count =0;
+        while(r<nums.size()-1){
+            int max_reach = r;
+            for(int i=l;i<=r;i++){
+                max_reach = max(max_reach,i+nums[i]);
+
+            }
+            l = r+1;
+            r= max_reach;
+            count++;
+        }
+        return count;
+
+
+
+
+    }
+};
